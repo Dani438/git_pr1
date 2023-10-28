0072d84ded00acdc4e7950904b101ac6c9231aa8
Dani <daniel.aescobar@hotmail.com>
2023-10-28 13:02:11 -0300

    Funciona Server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+
+require('dotenv').config({path:'./.env'});
+
+const puerto = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(puerto,()=>{
+    console.log('SERVER RUNING IN PORT: http://localhost:'+ puerto);
+
+});
