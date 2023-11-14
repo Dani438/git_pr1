dc7c59bc836cd58f8893779f4d00bfeabe547420
Dani <daniel.aescobar@hotmail.com>
2023-11-14 12:07:47 -0300

    Se agrega la pagina Ofertas

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
+//const path = require('path');
 require('dotenv').config({path:'./.env'});
 
 const puerto = process.env.PORT || 3000;
 
+
+//middelwares: funciones que estan entre la peticion y la respuesta
+const miMiddelware = (req, res, next) => {
+    console.log("Hola soy un middelware");
+    next();
+};
+
+
+app.use(miMiddelware)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile('index.html');
+});
+
+app.get('/ofertas', (req, res)=>{
+    res.sendFile('ofertas.html');
 });
 
 app.listen(puerto,()=>{
